26bb643bd7a2425cae1c5bfd3259dd1587e519e9
DiegoPre <arturopre@hotmail.com>
2023-09-07 18:50:05 -0500

    FIX PRE A PREC

--- Parcial1/src/app/app.component.ts
+++ Parcial1/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Parcial1 DIEGO PRE';
+  title = 'Parcial1 DIEGO PREC';
 
 }
